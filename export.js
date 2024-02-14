aaed87bb91a6220dd1b979bdab4e7ba9d967830c
FerreiroAlberto <homelandz@gmail.com>
2024-02-14 18:34:37 +0100

    Upload first challenge

---
+++ export.js
@@ -0,0 +1,28 @@
+/*
+export function strictEquals2(a, b) {
+  if (!a && !b) {
+    return !Object.is(a, b);
+  } else {
+    return Object.is(a, b);
+  }
+}
+
+strictEquals2();
+*/
+
+export function strictEquals(a, b) {
+  if (Object.is(a, NaN) && Object.is(b, NaN)) {
+    return false;
+  }
+  if (
+    (Object.is(a, 0) && Object.is(-0, b)) ||
+    (Object.is(a, -0) && Object.is(0, b))
+  ) {
+    return true;
+  }
+  if (Object.is(a, b)) {
+    return true;
+  }
+
+  return false;
+}
