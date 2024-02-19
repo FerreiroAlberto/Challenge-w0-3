9d4415707551f9255d5e84a44126a81b12217550
FerreiroAlberto <homelandz@gmail.com>
2024-02-19 15:46:44 +0100

    "Final upload"

--- export.js
+++ export.js
@@ -1,14 +1,12 @@
-/*
-export function strictEquals2(a, b) {
-  if (!a && !b) {
-    return !Object.is(a, b);
-  } else {
-    return Object.is(a, b);
-  }
-}
+//export function strictEquals2(a, b) {
+// if (!a && !b) {
+// return !Object.is(a, b);
+//} else {
+//return Object.is(a, b);
+//}
+//}
 
-strictEquals2();
-*/
+//strictEquals2();
 
 export function strictEquals(a, b) {
   if (Object.is(a, NaN) && Object.is(NaN, b)) {
@@ -21,8 +19,18 @@ export function strictEquals(a, b) {
     return true;
   }
   if (Object.is(a, b)) {
+    //este código sobra, me está diciendo que si Object.is(a,b) devuelve true, me de (en el return, true), es innecesario
     return true;
   }
 
   return false;
 }
+
+// de la línea 21 a la 26 se puede sustituir por  return Object.is(a,b)
+
+/* function strictBest (a,b) {
+if (Object.is(a,NaN)) return false;
+if (!a && !b) return true;
+return Object.is(a,b);
+}
+*/
